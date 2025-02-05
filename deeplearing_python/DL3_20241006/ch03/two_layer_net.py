746bc6e2de91eda0031efc5eab2440effa91533a
parkjiyon7 <parkjiyonaffair@gmail.com>
2025-02-05 12:48:52 +0900

    update deeplearning

--- deeplearing_python/DL3_20241006/ch03/two_layer_net.py
+++ deeplearing_python/DL3_20241006/ch03/two_layer_net.py
@@ -1,1 +1,97 @@
 # coding: utf-8
+import sys, os
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from common.functions import *
+from common.gradient import numerical_gradient
+import numpy as np
+
+
+
+class TwoLayerNet:
+
+
+    # 신경망 가중치 초기화
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    # 신경망의 출력값을 계산하는 함수
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    # 신경망의 손실값을 계산하는 함수
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    # 예측의 정확도를 계산하는 함수
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    # 가중치에 대한 수치 미분을 수행하는 함수
+    # x: 입력데이터, t: 정답 레이블
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+    # 가중치에 대한 역전파 방식을 수행하는 함수
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        #forward
+        a1 = np.dot(x, W1) +b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) +b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y-t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
+
+        return grads
+
+
+
+
+
+
+
+
+
